be76619ce99b300ee903d26e18ec17c02168999d
Ulises Zuniga <ulisestm3@gmail.com>
2024-11-11 22:03:03 -0600

    [UPDATE] Diseño - Buenas Practicas - Pistas de Auditoria

--- app.py
+++ app.py
@@ -1,19 +1,57 @@
-from flask import Flask, render_template, request, redirect,session, send_from_directory
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask import Flask, render_template, request, redirect, session, send_from_directory, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from datetime import datetime
 import config
 import pymysql
 import os
 
 app = Flask(__name__)
-
 app.secret_key = config.HEX_SEC_KEY
+
+# Configuración de Flask-Login
+login_manager_app = LoginManager(app)
+login_manager_app.login_view = 'admin_login'
+
+# Clase de usuario para manejar la autenticación
+class User(UserMixin):
+    def __init__(self, id_usuario, usuario, password, nombre, apellido, email):
+        self.id_usuario = id_usuario
+        self.usuario = usuario
+        self.password = password
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+
+    # Implementación de get_id para que Flask-Login pueda obtener el identificador del usuario
+    def get_id(self):
+        return str(self.id_usuario)
+
+    @staticmethod
+    def get(id_usuario):
+        conexion = dbconnection()
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE id_usuario = %s", (id_usuario,))
+        usuario_data = cursor.fetchone()
+        conexion.close()
+        if usuario_data:
+            return User(id_usuario=usuario_data[0], usuario=usuario_data[1], password=usuario_data[2], nombre=usuario_data[3], apellido=usuario_data[4], email=usuario_data[5])
+        return None
 
 # Función para establecer la conexión a la base de datos
 def dbconnection():
-    return pymysql.connect(host= config.MSQL_HOST, user= config.MSQL_USER, password= config.MSQL_PASSWORD, database= config.MSQL_DB)
+    return pymysql.connect(
+        host=config.MSQL_HOST,
+        user=config.MSQL_USER,
+        password=config.MSQL_PASSWORD,
+        database=config.MSQL_DB
+    )
 
-#Ruta para cargar imagenes y videos
+@login_manager_app.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+# Ruta para cargar imágenes y videos
+@app.route('/img/<imagen>')
 @app.route('/img/<imagen>')
 def imagenes(imagen):
     print(imagen)
@@ -24,33 +62,41 @@ def videos(video):
     print(video)
     return send_from_directory(os.path.join('templates/sitio/video'),video)
 
-# Rutas para sitio
+# Rutas del sitio público
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
 
-
 @app.route('/productos')
 def productos():
     conexion = dbconnection()
-    cursor=conexion.cursor()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM productos")
-    productos=cursor.fetchall()
-    conexion.commit()    
-    
+    productos = cursor.fetchall()
+    conexion.close()
     return render_template('sitio/productos.html', productos=productos)
 
 @app.route('/detalleproductos', methods=['POST'])
 def detalleproductos():
     _id = request.form['txtID']
     conexion = dbconnection()
-    cursor=conexion.cursor()
-    sql = 'SELECT * FROM productos WHERE id= %s'
-    cursor.execute(sql, _id)
-    productos=cursor.fetchall()
-    conexion.commit()
-
-    return render_template('sitio/detalleproductos.html', productos=productos)
+    cursor = conexion.cursor()
+    
+    # Consulta con INNER JOIN entre productos y usuarios a través de id_usuario
+    query = """
+        SELECT productos.*, usuarios.*
+        FROM productos
+        INNER JOIN usuarios ON productos.id_usuario = usuarios.id_usuario
+        WHERE productos.id = %s
+    """
+    
+    cursor.execute(query, (_id,))
+    resultado = cursor.fetchall()
+    
+    # Cierra la conexión
+    conexion.close()
+    
+    return render_template('sitio/detalleproductos.html', productos=resultado)
 
 @app.route('/nosotros')
 def nosotros():
@@ -64,17 +110,29 @@ def pago():
 def pago_exitoso():
     return render_template('sitio/pagoexitoso.html')
 
-# Rutas para admin
+# Rutas para administración
 @app.route('/admin')
+@login_required
 def index_productos():
     conexion = dbconnection()
-    cursor=conexion.cursor()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM productos")
-    productos=cursor.fetchall()
-    conexion.commit()    
+    productos = cursor.fetchall()
+    conexion.close()
     return render_template('admin/index.html', productos=productos)
 
+@app.route('/admin/')
+def false_admin():
+    return redirect(url_for('admin_login'))
+
+@app.route('/admin/logout')
+def logout_admin():
+    logout_user()
+    session.clear()
+    return redirect(url_for('admin_login'))
+
 @app.route('/admin/nosotros')
+@login_required
 def admin_nosotros():
     return render_template('admin/nosotros.html')
 
@@ -82,28 +140,23 @@ def admin_nosotros():
 def admin_login():
     return render_template('admin/login.html')
 
-
 @app.route('/admin/login', methods=['POST'])
 def admin_login_post():
-    _usuario=request.form['txtUsuario']
-    _password=request.form['txtPassword']
-    print(_usuario)
-    print(_password)
-    
+    _usuario = request.form['txtUsuario']
+    _password = request.form['txtPassword']
     conexion = dbconnection()
-    cursor=conexion.cursor()
-    cursor.execute("SELECT * FROM usuarios WHERE usuario = %s AND password = %s", (_usuario, _password))
-    usuario=cursor.fetchone()
-    print(usuario)
-        
-    
+    cursor = conexion.cursor()
+    cursor.execute("SELECT id_usuario, usuario, password, nombre, apellido, email FROM usuarios WHERE usuario = %s AND password = %s", (_usuario, _password))
+    usuario = cursor.fetchone()
+    conexion.close()
+
     if usuario:
         session['logeado'] = True
-        session['id'] = usuario[0]
-        return render_template('/admin/index.html')
-    
+        user = User(id_usuario=usuario[0], usuario=usuario[1], password=usuario[2], nombre=usuario[3], apellido=usuario[4], email=usuario[5])
+        login_user(user)
+        return redirect('/admin')
     else:
-        return render_template('/admin/login.html', message = " Credenciales no son correctas " )
+        return render_template('admin/login.html', message="Credenciales incorrectas")
 
 @app.route('/admin/registro')
 def admin_registro():
@@ -111,64 +164,64 @@ def admin_registro():
 
 @app.route('/admin/registro/usuario', methods=['POST'])
 def admin_registro_usuario():
-    conexion = None
-    
     try:
         _usuario = request.form['txtUsuario']
         _password = request.form['txtPassword']
         _nombre = request.form['txtNombre']
         _apellido = request.form['txtApellido']
         _email = request.form['txtEmail']
-
+        
+        # Obtiene la fecha y hora actual
+        fecha_registro = datetime.now()
+        
         conexion = dbconnection()
         cursor = conexion.cursor()
-
-        # Verificar si el usuario o el correo electrónico ya existen
-        sql_select = "SELECT * FROM usuarios WHERE usuario = %s"
-        cursor.execute(sql_select, (_usuario,))
+        
+        # Verifica si el usuario ya existe
+        cursor.execute("SELECT * FROM usuarios WHERE usuario = %s", (_usuario,))
         usuario_existente = cursor.fetchone()
-
+        
         if usuario_existente:
-            # Si el usuario o el correo electrónico ya existen, mostrar un mensaje en el formulario de registro
             return render_template('admin/registro.html', mensaje='El usuario ya existe.')
-
-        # Si el usuario y el correo electrónico no existen, insertar los datos en la base de datos
-        sql_insert = "INSERT INTO usuarios (usuario, password, nombre, apellido, email) VALUES (%s, %s, %s, %s, %s)"
-        cursor.execute(sql_insert, (_usuario, _password, _nombre, _apellido, _email))
+        
+        # Inserta el nuevo usuario con la fecha de registro
+        cursor.execute("""
+            INSERT INTO usuarios (usuario, password, nombre, apellido, email, fecha_registro) 
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, 
+            (_usuario, _password, _nombre, _apellido, _email, fecha_registro)
+        )
+        
         conexion.commit()
-
-        # Redireccionar a la página de inicio de sesión después del registro exitoso
+        conexion.close()
+        
         return render_template('admin/login.html')
-
+    
     except pymysql.Error as e:
         print("Error de MySQL:", e)
-    finally:
-        if conexion is not None:
-            conexion.close()
-
-    return render_template('admin/login.html')
+    
+    return render_template('admin/registro.html')
 
 
 @app.route('/admin/productos')
+@login_required
 def admin_productos_leer():
-    
-    try:
-        conexion = dbconnection()
-        cursor=conexion.cursor()
-        cursor.execute("SELECT * FROM productos")
-        productos=cursor.fetchall()
-        conexion.commit()
-        print(productos)
-        conexion.close()
-    except pymysql.Error as e:
-        print("Error de MySQL:", e)
+    conexion = dbconnection()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM productos")
+    productos = cursor.fetchall()
+    conexion.close()
     return render_template('admin/productos.html', productos=productos)
 
 @app.route('/admin/editar')
+@login_required
 def admin_productos_editar():
+    # Tomar el id_usuario del usuario autenticado
+    _id_usuario = current_user.id_usuario
     conexion=dbconnection()
     cursor=conexion.cursor()
-    cursor.execute("select * from productos")
+    sql = "SELECT * FROM productos WHERE id_usuario = %s"
+    cursor.execute(sql, (_id_usuario,))
     producto=cursor.fetchall()
 
     insertObjeto=[]
@@ -181,53 +234,9 @@ def admin_productos_editar():
     print(producto)
     return render_template('admin/editar.html', producto=insertObjeto)
 
-
-@app.route('/admin/productos/guardaradv', methods=['POST'])
-def admin_productos_guardaradv():
-    try:
-        conexion = dbconnection()
-        cursor = conexion.cursor()
-        _nombre = request.form['txtNombre']
-        _archivo = request.files['txtImagen']
-        _precio = request.form['txtPrecio']
-
-        tiempo= datetime.now()
-        horaActual=tiempo.strftime('%Y-%m-%d_%H-%M-%S')
-
-        if _archivo.filename!="":
-            nuevoNombre=horaActual+"_"+_archivo.filename
-            _archivo.save("templates/sitio/img/"+nuevoNombre)
-                
-        sql = "INSERT INTO productos (nombre, imagen, precio) VALUES (%s, %s, %s)"
-        cursor.execute(sql, (_nombre, nuevoNombre, _precio))
-        conexion.commit()
-
-        print(_nombre)
-        print(_archivo)
-        print(_precio)
-
-    except pymysql.Error as e:
-        print("Error de MySQL:", e)
-    finally:
-        conexion.close()
-
-    conexion=dbconnection()
-    cursor=conexion.cursor()
-    cursor.execute("select * from productos")
-    producto=cursor.fetchall()
-
-    insertObjeto=[]
-    columnaNames=[column[0] for column in cursor.description]
-
-    for record in producto:
-        insertObjeto.append(dict(zip(columnaNames, record)))
-    cursor.close()
-        
-    print(producto)
-    return render_template('admin/editar.html', producto=insertObjeto)
-
-
-@app.route('/admin/productos/guardar', methods=['POST'])
+# Rutas para CRUD de productos
+@app.route('/admin/productos/guardar', methods=['GET','POST'])
+@login_required
 def admin_productos_guardar():
     try:
         conexion = dbconnection()
@@ -240,92 +249,60 @@ def admin_productos_guardar():
         _moneda = request.form['txtMoneda']
         _precio = request.form['txtPrecio']
         _descripcion = request.form['txtDescripcion']
+        
+        # Tomar el id_usuario del usuario autenticado
+        _id_usuario = current_user.id_usuario
+        _id_usuario_actualiza = current_user.id_usuario
+        fecha_registro = datetime.now()
+        fecha_actualizacion = datetime.now()
 
         nuevoNombre1 = ""
         nuevoNombre2 = ""
         nuevoNombre3 = ""
-        nuevoNombrevideo =""
+        nuevoNombrevideo = ""
 
         tiempo= datetime.now()
         horaActual=tiempo.strftime('%Y-%m-%d_%H-%M-%S')
-        
-        if _archivo1.filename!="":
-            nuevoNombre1=horaActual+"_"+_archivo1.filename
-            _archivo1.save("templates/sitio/img/"+nuevoNombre1)
 
-        if _archivo2.filename!="":
-            nuevoNombre2=horaActual+"_"+_archivo2.filename
-            _archivo2.save("templates/sitio/img/"+nuevoNombre2)
+        # Agregar logs para verificar que los archivos están siendo recibidos
+        print("Archivos recibidos:", request.files)
 
-        if _archivo3.filename!="":
-            nuevoNombre3=horaActual+"_"+_archivo3.filename
-            _archivo3.save("templates/sitio/img/"+nuevoNombre3)
+        if _archivo1.filename != "":
+            nuevoNombre1 = horaActual + "_" + _archivo1.filename
+            print("Guardando imagen 1 con nombre:", nuevoNombre1)
+            _archivo1.save(os.path.join(app.root_path, "templates/sitio/img/", nuevoNombre1))
 
-        if _video.filename!="":
-            nuevoNombrevideo=horaActual+"_"+_video.filename
-            _video.save("templates/sitio/video/"+nuevoNombrevideo)
-                
-        sql = "INSERT INTO productos (nombre, imagen1, imagen2, imagen3, video, precio, descripcion, moneda) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        cursor.execute(sql, (_nombre, nuevoNombre1, nuevoNombre2, nuevoNombre3, nuevoNombrevideo, _precio,_descripcion, _moneda))
-        conexion.commit() 
+        if _archivo2.filename != "":
+            nuevoNombre2 = horaActual + "_" + _archivo2.filename
+            print("Guardando imagen 2 con nombre:", nuevoNombre2)
+            _archivo2.save(os.path.join(app.root_path, "templates/sitio/img/", nuevoNombre2))
+
+        if _archivo3.filename != "":
+            nuevoNombre3 = horaActual + "_" + _archivo3.filename
+            print("Guardando imagen 3 con nombre:", nuevoNombre3)
+            _archivo3.save(os.path.join(app.root_path, "templates/sitio/img/", nuevoNombre3))
+
+        if _video.filename != "":
+            nuevoNombrevideo = horaActual + "_" + _video.filename
+            print("Guardando video con nombre:", nuevoNombrevideo)
+            _video.save(os.path.join(app.root_path, "templates/sitio/video/", nuevoNombrevideo))
+
+        sql = """INSERT INTO productos (nombre, imagen1, imagen2, imagen3, video, precio, descripcion, moneda, id_usuario, fecha_registro, fecha_actualizacion, id_usuario_actualiza) 
+                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        cursor.execute(sql, (_nombre, nuevoNombre1, nuevoNombre2, nuevoNombre3, nuevoNombrevideo, _precio, _descripcion, _moneda, _id_usuario, fecha_registro, fecha_actualizacion, _id_usuario_actualiza))
+        conexion.commit()
 
     except pymysql.Error as e:
         print("Error de MySQL:", e)
+    except Exception as e:
+        print("Error general:", e)  # Captura otros posibles errores.
     finally:
         conexion.close()
 
     return redirect('/admin/editar')
 
-@app.route('/admin/productos/borrar', methods=['POST'])
-def admin_productos_borrar():
-    _id = request.form['txtID']
-    print(_id) 
-    
-    conexion=dbconnection()
-    cursor=conexion.cursor()
-    sql = 'delete from productos where id=%s'
-    cursor.execute(sql, _id)
-    conexion.commit()
-
-    return redirect('/admin/editar')
-
-@app.route('/admin/productos/borraradv', methods=['POST'])
-def admin_productos_borraradv():
-    _id = request.form['txtID']
-    print(_id)
-    
-    conexion=dbconnection()
-    cursor=conexion.cursor()
-    cursor.execute("select imagen from productos where id=%s", (_id))
-    producto=cursor.fetchall()
-    conexion.commit()
-    print(producto)
-
-    if os.path.exists("templates/sitio/img/"+str(producto[0][0])):
-        os.unlink("templates/sitio/img/"+str(producto[0][0]))
-
-    conexion=dbconnection()
-    cursor=conexion.cursor()
-    cursor.execute("delete from productos where id=%s", (_id))
-    conexion.commit()
-
-    conexion=dbconnection()
-    cursor=conexion.cursor()
-    cursor.execute("select * from productos")
-    producto=cursor.fetchall()
-
-    insertObjeto=[]
-    columnaNames=[column[0] for column in cursor.description]
-
-    for record in producto:
-        insertObjeto.append(dict(zip(columnaNames, record)))
-    cursor.close()
-        
-    print(producto)
-    return render_template('admin/editar.html', producto=insertObjeto)
-
-
 @app.route('/admin/productos/actalizar', methods=['POST'])
+@login_required
 def admin_productos_actualizar():
     conexion = dbconnection()
     cursor = conexion.cursor()
@@ -339,6 +316,9 @@ def admin_productos_actualizar():
     _precio = request.form['txtPrecio']
     _descripcion = request.form['txtDescripcion']
 
+    # Tomar el id_usuario del usuario autenticado
+    _id_usuario = current_user.id_usuario
+
     nuevoNombre1 = ""
     nuevoNombre2 = ""
     nuevoNombre3 = ""
@@ -349,28 +329,28 @@ def admin_productos_actualizar():
     
     if _archivo1.filename!="":
         nuevoNombre1=horaActual+"_"+_archivo1.filename
-        _archivo1.save("templates/sitio/img/"+nuevoNombre1)
+        _archivo1.save(os.path.join(app.root_path,"templates/sitio/img/"+nuevoNombre1))
     else:
         nuevoNombre1= request.form['txtImagen11']
 
 
     if _archivo2.filename!="":
         nuevoNombre2=horaActual+"_"+_archivo2.filename
-        _archivo2.save("templates/sitio/img/"+nuevoNombre2)
+        _archivo2.save(os.path.join(app.root_path,"templates/sitio/img/"+nuevoNombre2))
     else:
         nuevoNombre2 = request.form['txtImagen21']
 
 
     if _archivo3.filename!="":
         nuevoNombre3=horaActual+"_"+_archivo3.filename
-        _archivo3.save("templates/sitio/img/"+nuevoNombre3)
+        _archivo3.save(os.path.join(app.root_path,"templates/sitio/img/"+nuevoNombre3))
     else:
         nuevoNombre3 = request.form['txtImagen31']
 
 
     if _video.filename!="":
         nuevoNombrevideo=horaActual+"_"+_video.filename
-        _video.save("templates/sitio/video/"+nuevoNombrevideo)
+        _video.save(os.path.join(app.root_path,"templates/sitio/video/"+nuevoNombrevideo))
     else:
         nuevoNombrevideo = request.form['txtVideo1']
 
@@ -382,7 +362,8 @@ def admin_productos_actualizar():
     
     conexion=dbconnection()
     cursor=conexion.cursor()
-    cursor.execute("select * from productos")
+    sql = "select * from productos where id_usuario=%s"
+    cursor.execute(sql, (_id_usuario))
     producto=cursor.fetchall()
 
     insertObjeto=[]
@@ -394,8 +375,28 @@ def admin_productos_actualizar():
         
     print(producto)
     return render_template('admin/editar.html', producto=insertObjeto)
-    
 
-#Ejecuta la app
-if __name__ =='__main__':
+@app.route('/admin/productos/borrar', methods=['POST'])
+@login_required
+def admin_productos_borrar():
+    _id = request.form['txtID']
+    print(_id) 
+    
+    conexion=dbconnection()
+    cursor=conexion.cursor()
+    sql = 'delete from productos where id=%s'
+    cursor.execute(sql, _id)
+    conexion.commit()
+
+    return redirect('/admin/editar')
+
+@app.route('/admin/logout')
+@login_required
+def admin_logout():
+    session.pop('logeado', None)
+    logout_user()
+    return redirect('/admin/login')
+
+# Ejecuta la app
+if __name__ == '__main__':
     app.run(debug=True)
